d152fd17418cd55f6131bec65c350121d4f8aeda
Sergio García Díaz <sergady987@gmail.com>
2021-11-02 20:19:53 +0100

    Clase RedditComment creada

--- zstReader.py
+++ zstReader.py
@@ -1,6 +1,7 @@
 import zstandard
 import re 
 
+# Comprueba que cogemos lo que nos interesa de los datos
 def correct(data):
     if(data.find(b'selftext') > 0): # va hasta send replies
         if(not (data.find(b'[removed]') > 0 or data.find(b'[deleted]') > 0)):
@@ -17,11 +18,18 @@ with open("RS_2019-09.zst", 'rb') as fh:
         chunk = reader.read(16384)
         data = chunk.split(b',')
         for j in range(len(data)):
-            if(correct(data[j])):
-                print(data[j].decode('UTF-8'))
+            #if(correct(data[j])):
+            print(data[j].decode('UTF-8'))
         if not chunk:
             break
 
 
-    
-# va hasta
+
+# Debería comprobar: "subreddit":"DirtyScatPals"
+# Luego la información se suele encontrar en "selftext":"[...]"
+
+class RedditComment:
+    def __init__(self, self_text, subreddit):
+        self.self_text = self_text
+        self.subreddit = subreddit
+        
