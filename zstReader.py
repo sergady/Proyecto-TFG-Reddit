1441b9994564bfa58ca45b8a97e3ca0e16288ca6
Sergio García Díaz <sergady987@gmail.com>
2021-11-02 21:51:26 +0100

    JSON funciona

--- zstReader.py
+++ zstReader.py
@@ -12,12 +12,14 @@ def correct(data):
     else:
         return False
 
+
+
 with open("RS_2019-09.zst", 'rb') as fh:
     dctx = zstandard.ZstdDecompressor()
     reader = dctx.stream_reader(fh)
     #while True:
-    chunk = reader.read(16384)
-    chunk = chunk.decode('UTF-8') # Cambia de byte-like a string
+    chunk = reader.read(16384) # I need to be careful with this because it cuts jsons by half
+    chunk = chunk.decode('UTF-8') # Changes byte-like to string
     data = chunk.split('\n')
     data_dict = json.loads(data[4])
     print((data_dict['selftext']))
