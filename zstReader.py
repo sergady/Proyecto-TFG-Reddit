10a443891ed001c8ba02a93662cd6e79ee69adba
Sergio García Díaz <sergady987@gmail.com>
2021-11-07 19:44:08 +0100

    Code improvement

--- zstReader.py
+++ zstReader.py
@@ -3,8 +3,8 @@ import json
 import re 
 import time
 
+file_name = "RS_2019-09.zst"
 
-reddit_comment_list = []
 
 class RedditComment:
     def __init__(self, self_text, subreddit):
@@ -17,29 +17,37 @@ def checkSelfText(self_text):
     return True
 
 start = time.time()
-# Open the file as fh
-with open("RS_2019-09.zst", 'rb') as fh:
-    dctx = zstandard.ZstdDecompressor()
-    reader = dctx.stream_reader(fh)
-    i = 0
-    while True:
-        # We read the data and save it into chunks
-        chunk = reader.read(20000) # I need to be careful with this because it cuts jsons by half
-        chunk = chunk.decode('UTF-8') # Changes byte-like to string
-        data = chunk.split('\n') # Divides the text into posts
-        for each in data:
-            i += 1
-            try:
-                data_dict = json.loads(each)
-                if(checkSelfText(data_dict['selftext'])):
-                    reddit_comment_list.append(RedditComment(data_dict['selftext'], data_dict['subreddit']))
-                if(i%100000 == 0):
-                    print('%d posts read' % i)
-            except json.decoder.JSONDecodeError:
-                continue # Seems like we do this to avoid errors but it is to eliminate divided posts
-        if not chunk:
-            break
 
+def readData(file_name):
+    reddit_comment_list = []
+    # Open the file as fh
+    with open(file_name, 'rb') as fh:
+        dctx = zstandard.ZstdDecompressor()
+        reader = dctx.stream_reader(fh)
+        i = 0
+        while True:
+            # We read the data and save it into chunks
+            chunk = reader.read(20000) # I need to be careful with this because it cuts jsons by half
+            chunk = chunk.decode('UTF-8') # Changes byte-like to string
+            data = chunk.split('\n') # Divides the text into posts
+            for each in data:
+                i += 1
+                try:
+                    data_dict = json.loads(each)
+                    if(checkSelfText(data_dict['selftext'])):
+                        reddit_comment_list.append(RedditComment(data_dict['selftext'], data_dict['subreddit']))
+                    if(i%100000 == 0):
+                        print('%d posts read' % i)
+                except json.decoder.JSONDecodeError:
+                    continue # Seems like we do this to avoid errors but it is to eliminate divided posts
+            if not chunk:
+                break
+
+    return reddit_comment_list
+ 
+
+def cleanComments(reddit_comment_list):
+    
 
 end = time.time()
 print('Time: ', end - start)
