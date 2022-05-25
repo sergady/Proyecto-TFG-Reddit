5004f454105abaa63ac1f07972bd74cf6a0427a9
Garcia, Sergio <sergio.garcia@sky.uk>
2022-05-25 18:24:29 +0100

    Basic submissions gather complete

--- reddit_gathering/gather_submissions.py
+++ reddit_gathering/gather_submissions.py
@@ -7,5 +7,8 @@ import json
 subreddit = "NarcissisticAbuse"
 endpoint = "https://api.pushshift.io/reddit/submission/search/?subreddit="+subreddit+"&after=2019-01-01&before=2019-12-31"
 result = requests.get(endpoint) 
-data = json.loads(result. text, strict=False) 
-print(data['data'])
+data = json.loads(result.text, strict=False) 
+#print(data['data'])
+
+for submission in data['data']:
+    print(submission)
