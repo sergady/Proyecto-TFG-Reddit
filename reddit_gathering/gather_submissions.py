2dbbba35d94b08119bfcde3c738ade40ac78f422
Garcia, Sergio <sergio.garcia@sky.uk>
2022-05-26 10:30:09 +0100

    Starting to gather the results of each subreddit

--- reddit_gathering/gather_submissions.py
+++ reddit_gathering/gather_submissions.py
@@ -2,13 +2,13 @@ from psaw import PushshiftAPI
 import requests
 import json
 
-#api = PushshiftAPI()
-#results = list(api.search_submissions(subreddit=100))
-subreddit = "NarcissisticAbuse"
-endpoint = "https://api.pushshift.io/reddit/submission/search/?subreddit="+subreddit+"&after=2019-01-01&before=2019-12-31"
-result = requests.get(endpoint) 
-data = json.loads(result.text, strict=False) 
-#print(data['data'])
 
-for submission in data['data']:
+def get_data_from_subreddit(subreddit):
+    endpoint = "https://api.pushshift.io/reddit/submission/search/?subreddit="+subreddit+"&after=2019-01-01&before=2019-12-31"
+    result = requests.get(endpoint) 
+    data = json.loads(result.text, strict=False) 
+    return data['data']
+
+
+for submission in get_data_from_subreddit('abuse'):
     print(submission)
