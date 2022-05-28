8ac4f0f99245c7d24d4eeef983075e453be1328f
Garcia, Sergio <sergio.garcia@sky.uk>
2022-05-28 08:59:58 +0100

    Relative path error needs fix

--- reddit_gathering/gather_submissions.py
+++ reddit_gathering/gather_submissions.py
@@ -1,14 +1,24 @@
-from psaw import PushshiftAPI
+from RedditPost import RedditPost
 import requests
 import json
 
-
+# Gathers the data from a subreddit between the first day and the last of 2019
 def get_data_from_subreddit(subreddit):
     endpoint = "https://api.pushshift.io/reddit/submission/search/?subreddit="+subreddit+"&after=2019-01-01&before=2019-12-31"
     result = requests.get(endpoint) 
     data = json.loads(result.text, strict=False) 
     return data['data']
 
+def transform_data_into_objects(data):
+    posts = list()
+    for submission in data:
+        posts.append(RedditPost.RedditPost( submission['id'], submission['created_utc'], submission['title'], submission['author'], submission['selftext'], submission['subreddit']))
+        #print(submission)
 
-for submission in get_data_from_subreddit('abuse'):
-    print(submission)
+    return posts
+
+def main():
+    posts = transform_data_into_objects(get_data_from_subreddit('abuse'))
+    print(len(posts))
+
+main()
