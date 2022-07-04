08cb79fe97876d5d168709a1ae85a143a4c58be4
Sergio García Díaz <45501321+sergady@users.noreply.github.com>
2022-07-04 15:56:10 +0200

    Removed code duplicated

--- clustering/Prepare_classifier_training_data.py
+++ clustering/Prepare_classifier_training_data.py
@@ -5,6 +5,23 @@ import Prepare_clasification_sample as sample_gen
 from tqdm import tqdm
 
 
+def load_post_control(post):
+    post = json.loads(post)
+    text = post["title"] + " " + post["self_text"]
+    text = removeSymbolsAndUrls(text)
+
+    terminos = text.split()
+    
+    texto = " ".join(terminos)
+    texto = "__label__control " + texto
+    return texto
+
+def load_post_label(post):
+    terminos = post.split()
+    texto = " ".join(terminos)
+    texto = "__label__" + topic +" " + texto
+    return texto
+
 [texts,tests] = sample_gen.return_train_sample()
 
 documentos_entrenamiento = list()
@@ -13,28 +30,14 @@ documentos_test = list()
 with tqdm(total=len(texts)) as barra:
     for topic in texts:
         for post in texts[topic]:
-            terminos = post.split()
-            
-            texto = " ".join(terminos)
-            texto = "__label__" + topic +" " + texto
-
-            documentos_entrenamiento.append(texto)
+            documentos_entrenamiento.append(load_post_label(post))
         barra.update(1)
 
 control_texts = io.open("data/random_posts_control.ndjson", mode="r", encoding="utf-8").readlines()
 
 with tqdm(total=len(control_texts)) as barra:
     for post in control_texts:
-        post = json.loads(post)
-        text = post["title"] + " " + post["self_text"]
-        text = removeSymbolsAndUrls(text)
-
-        terminos = text.split()
-        
-        texto = " ".join(terminos)
-        texto = "__label__control " + texto
-
-        documentos_entrenamiento.append(texto)
+        documentos_entrenamiento.append(load_post_control(post))
         barra.update(1)
 
 documentos_entrenamiento = "\n".join(documentos_entrenamiento)
@@ -44,28 +47,14 @@ io.open("data/documentos_entrenamiento.txt", mode="w", encoding="utf-8").write(d
 with tqdm(total=len(tests)) as barra:
     for topic in tests:
         for post in tests[topic]:
-            terminos = post.split()
-            
-            texto = " ".join(terminos)
-            texto = "__label__" + topic +" " + texto
-
-            documentos_test.append(texto)
+            documentos_test.append(load_post_label(post))
         barra.update(1)
 
 control_test_texts = io.open("data/random_posts_control_test.ndjson", mode="r", encoding="utf-8").readlines()
 
 with tqdm(total=len(control_test_texts)) as barra:
     for post in control_test_texts:
-        post = json.loads(post)
-        text = post["title"] + " " + post["self_text"]
-        text = removeSymbolsAndUrls(text)
-
-        terminos = text.split()
-        
-        texto = " ".join(terminos)
-        texto = "__label__control " + texto
-
-        documentos_test.append(texto)
+        documentos_test.append(load_post_control(post))
         barra.update(1)
 
 documentos_test = "\n".join(documentos_test)
